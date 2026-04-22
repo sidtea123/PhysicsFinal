96ea53c48e6427279d90ada480f6a5fa2484fefa
Sid Tewari <stewari2@ncsu.edu>
2026-04-22 16:00:58 -0400

    skeleton

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-print('balls')
+import pygame
 
-for i in range(50):
-    print('diggin in mah butt')
-    print('and yo butt')
-#ben is a chud
-#chudzilla
+# visualizing
---
+++ particle.py
@@ -0,0 +1,5 @@
+class Particle:
+    def __init__(self, r, v, p):
+        self.r = r
+        self.v = v
+        self.p = p
